4674220ceb8a97b9389649127b656c22ba081371
MacBookPro <alteyaa>
2019-12-13 19:07:16 +0600

    commit

--- app/src/main/java/com/g/firstmeet/MainActivity.kt
+++ app/src/main/java/com/g/firstmeet/MainActivity.kt
@@ -1,17 +1,19 @@
 package com.g.firstmeet
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    private var mEditText: EditText? = null
-
+    private lateinit var mEditText: EditText
+    private lateinit var sendText: String
+    private lateinit var txtInfo: String
+    private val REQUEST_CODE = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,25 +26,30 @@ class MainActivity : AppCompatActivity() {
 
         saveBtn.setOnClickListener {
 
-            val mEdittextInfo = mEditText?.getText().toString()
-
-            if (mEdittextInfo.equals("")) {
-                mEditText?.error = "Field is empty"
-                Toast.makeText(applicationContext, "Please enter some message! ", Toast.LENGTH_SHORT).show()
-
-            } else {
-                val intent = Intent(this@MainActivity, SecondActivity::class.java)
-                intent.putExtra("Text", mEdittextInfo)
-                startActivity(intent)
-
-            }
-
-            var intent = intent
-            val txtInfo = intent.getStringExtra("SomeText")
-
-            val resultTv = findViewById<TextView>(R.id.resultTv)
-            resultTv.text = "Text:  " + txtInfo
-
+          send()
         }
     }
-}
+        private fun send(){
+        sendText = mEditText.text.toString()
+        if (notNull(sendText)){
+                val intent = Intent(this, SecondActivity::class.java)
+                intent.putExtra("text", sendText)
+                startActivityForResult(intent, REQUEST_CODE)
+            } else{
+                Toast.makeText(this, "Пожалуйста, заполните поле", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        private fun notNull(text: String):Boolean{
+            return text != ""
+        }
+
+        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            super.onActivityResult(requestCode, resultCode, data)
+            if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK){
+                txtInfo = data!!.getStringExtra("returnText")
+                if (!sendText.equals(txtInfo)){
+                    Toast.makeText(this, "Данные из Second Activity были изменены с $sendText на $txtInfo", Toast.LENGTH_LONG).show()
+                }
+            }
+}}
--- app/src/main/java/com/g/firstmeet/SecondActivity.kt
+++ app/src/main/java/com/g/firstmeet/SecondActivity.kt
@@ -1,46 +1,53 @@
 package com.g.firstmeet
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
+
+
 
 class SecondActivity : AppCompatActivity() {
 
 
-    private var mEditTxt2: EditText? = null
+    private lateinit var mEditTxt2: EditText
+    private lateinit var saveBtn: Button
+    private lateinit var infoTv: String
+    private lateinit var txtInfo: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
 
-          mEditTxt2 = findViewById<EditText>(R.id.edtTxtSecond)
-        val saveBtn2 = findViewById<Button>(R.id.btnSave2)
+        mEditTxt2 = findViewById<EditText>(R.id.edtTxtSecond)
+        saveBtn = findViewById<Button>(R.id.btnSave2)
 
+        saveBtn.setOnClickListener {
 
-        saveBtn2.setOnClickListener {
-
-            val txtInfo = mEditTxt2?.getText().toString()
-
-            if (txtInfo.equals("")) {
-                mEditTxt2?.error = "Field is empty"
-                Toast.makeText(applicationContext, "Please enter some message! ", Toast.LENGTH_SHORT).show()
-
-            } else {
-                val intent = Intent(this@SecondActivity, MainActivity::class.java)
-                intent.putExtra("SomeText", txtInfo)
-                startActivity(intent)
-            }
-            var intent = intent
-            val mEditTextInfo = intent.getStringExtra("Text")
-
-            val infoTv = findViewById<TextView>(R.id.txtInfo)
-            infoTv.text = "Text:" + mEditTextInfo
+            backInfo()
         }
+            infoTv = intent.getStringExtra("text")
+            mEditTxt2.setText(infoTv)
+
+        }
+
+        private fun backInfo (){
+             txtInfo = mEditTxt2.text.toString()
+
+            if (notNull(txtInfo)){
+                setResult(Activity.RESULT_OK, Intent(this, MainActivity::class.java).putExtra("Changing", txtInfo))
+                finish()
+            } else{
+                Toast.makeText(this, "Пожалуйста заполните поле", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    private fun notNull(text: String?):Boolean{
+        return text != null
 
     }
 }
